aebf56336aa6cbdaad000f402bcbbf97f55351ef
Nguyen Vuong <vlmnguyen98@gmail.com>
2019-09-16 10:41:15 +0700

    Implemented Resize with Interpolation

--- DigitalImageProcessing/ImageProcessingGUI/ImageOperations/AffineTransformations.py
+++ DigitalImageProcessing/ImageProcessingGUI/ImageOperations/AffineTransformations.py
@@ -1,5 +1,5 @@
 import numpy as np
-from cv2 import warpAffine, imread, imwrite
+import cv2
 from math import sin, cos, radians, pi, sqrt
 
 def translate(image, delta_x=0, delta_y=0):
@@ -8,7 +8,7 @@ def translate(image, delta_x=0, delta_y=0):
         [0, 1, delta_y]]
     )
     r, c = image.shape[:2]
-    return warpAffine(image, T, (c,r))
+    return cv2.warpAffine(image, T, (c,r))
 
 def rotate(image, theta=0, clockwise=False, scale='auto'):
     theta = radians(theta%360)
@@ -23,7 +23,7 @@ def rotate(image, theta=0, clockwise=False, scale='auto'):
         [[a, b, (1-a)*center_x-b*center_y], 
         [-b, a, b*center_x+(1-a)*center_y]]
     )
-    return warpAffine(image, T, (c,r))
+    return cv2.warpAffine(image, T, (c,r))
 
 def rescale(image, scale_x=1, scale_y=1):
     T = np.float32(
@@ -31,7 +31,7 @@ def rescale(image, scale_x=1, scale_y=1):
         [0, scale_y, 0]]
     )
     r, c = image.shape[:2]
-    return warpAffine(image, T, (c,r))
+    return cv2.warpAffine(image, T, (c,r))
 
 def shear(image, shear_x=0, shear_y=0):
     T = np.float32(
@@ -39,8 +39,12 @@ def shear(image, shear_x=0, shear_y=0):
         [shear_x, 1, 0]]
     )
     r, c = image.shape[:2]
-    return warpAffine(image, T, (c,r))
+    return cv2.warpAffine(image, T, (c,r))
 
-# if __name__ == "__main__":
-#     org = imread("original.png")
-#     imwrite("result.png", rotate(org,45))
+def resize(image, new_x, new_y, interpolation='NEAREST'):
+    inter_map = {
+        'NEAREST': cv2.INTER_NEAREST,
+        'BILINEAR': cv2.INTER_LINEAR,
+        'CUBIC': cv2.INTER_CUBIC
+    }
+    return cv2.resize(image, (new_x, new_y), interpolation=inter_map[interpolation])
